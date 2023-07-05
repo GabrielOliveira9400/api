3bfb0ce9e17d9a3d1c99f21ef155201504dc467b
Gabriel <gabriel.alves9400@hotmail.com>
2023-07-04 22:39:37 -0300

    alter

--- src/routes/reserves.ts
+++ src/routes/reserves.ts
@@ -2,7 +2,7 @@ import * as ReserveController from "../controllers/reserveController";
 import { Router } from 'express';
 const router = Router();
 
-router.get('/reserves/:email', ReserveController.reserveByEmail);
+router.get('/reserves/email/:email', ReserveController.reserveByEmail);
 router.get('/reserves/:_id', ReserveController.getReserve);
 router.post('/reserves', ReserveController.addReserve);
 
